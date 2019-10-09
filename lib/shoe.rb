f18b6045f59faeabf63b379b22dcddb574a1f28f
cedric e goddard <8ceg6@comcast.net>
2019-10-09 21:43:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,10 @@
 class Shoe
 attr_accessor  :color, :size, :material, :condition
 attr_reader :brands
-  brands[]
+
     def initialize(brand)
     @brand = brand
-    if !(brands.include?(@brand))
-      brands << @brand
+    
     end
 end
 end
