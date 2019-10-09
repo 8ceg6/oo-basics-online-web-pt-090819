b547a982b10e1ec93575d3e543ef8c6fecf2b423
cedric e goddard <8ceg6@comcast.net>
2019-10-09 21:48:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,10 +6,10 @@ attr_reader :brand
     def initialize(brand)
     @brand = brand
   end
-  def cobble()
-    if condition == new 
-      puts "Your shoe is as good as new"
-    else 
+def cobble()
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
   
 end
 
